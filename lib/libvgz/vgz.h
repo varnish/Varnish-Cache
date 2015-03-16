f02f13b0ef95276911b198b18a7e209373062745
Guillaume Quintard <guillaume.quintard@gmail.com>
2015-03-16 11:55:39 +0100

    z_stream_s can fit on 128 bytes

--- lib/libvgz/vgz.h
+++ lib/libvgz/vgz.h
@@ -85,11 +85,11 @@ struct internal_state;
 typedef struct z_stream_s {
     z_const Bytef *next_in;     /* next input byte */
     uInt     avail_in;  /* number of bytes available at next_in */
-    uLong    total_in;  /* total number of input bytes read so far */
-
-    Bytef    *next_out; /* next output byte should be put there */
     uInt     avail_out; /* remaining free space at next_out */
+
+    uLong    total_in;  /* total number of input bytes read so far */
     uLong    total_out; /* total number of bytes output so far */
+    Bytef    *next_out; /* next output byte should be put there */
 
     z_const char *msg;  /* last error message, NULL if no error */
     struct internal_state FAR *state; /* not visible by applications */
